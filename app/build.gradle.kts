01b8ad46aa074b01e2703324bca3b1a5d4e634ee
sock_magic <startxingcheng@gmail.com>
2024-10-23 00:03:17 +0800

    fix: 修复 ci 错误

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
         }
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
