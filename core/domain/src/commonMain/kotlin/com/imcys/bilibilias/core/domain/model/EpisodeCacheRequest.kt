468cbb54c296eb4918d6905deb794178944b638d
sock_magic <startxingcheng@gmail.com>
2025-09-16 23:08:50 +0800

    Refactor: Add videoType to EpisodeCacheRequest
    This commit adds a `videoType` field to the `EpisodeCacheRequest` data class. This allows specifying the type of video (e.g., normal, dash) when requesting an episode cache.
    
    Specific changes:
    - In `core/domain/src/commonMain/kotlin/com/imcys/bilibilias/core/domain/model/EpisodeCacheRequest.kt`:
        - Added a new field `videoType: VideoType` to the `EpisodeCacheRequest` data class.
        - Imported `com.imcys.bilibilias.core.model.VideoType`.

--- core/domain/src/commonMain/kotlin/com/imcys/bilibilias/core/domain/model/EpisodeCacheRequest.kt
+++ core/domain/src/commonMain/kotlin/com/imcys/bilibilias/core/domain/model/EpisodeCacheRequest.kt
@@ -1,5 +1,8 @@
 package com.imcys.bilibilias.core.domain.model
 
+import com.imcys.bilibilias.core.model.VideoType
+
 data class EpisodeCacheRequest(
     val cacheState: EpisodeCacheState,
+    val videoType: VideoType,
 )
