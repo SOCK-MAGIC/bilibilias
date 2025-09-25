f521f025d00715edb10caf5592f0d29e45c6e870
sock_magic <startxingcheng@gmail.com>
2025-09-25 21:00:49 +0800

    Refactor: Remove log filtering for "seg.so" paths
    This commit removes the log filtering logic that previously excluded requests containing "seg.so" in their URL path from being logged. Now, all HTTP requests will be logged regardless of their path.
    
    Specific changes:
    - In `core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/HttpLogging.kt`:
      - Removed the `filter` block that checked `request.url.encodedPath.contains("seg.so")`.

--- core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/HttpLogging.kt
+++ core/datasource/src/commonMain/kotlin/com/imcys/bilibilias/core/datasource/HttpLogging.kt
@@ -5,7 +5,6 @@ import io.ktor.client.HttpClientConfig
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.http.encodedPath
 
 internal fun HttpClientConfig<*>.HttpLogging() {
     val httpLogger = logger("BilibiliApi")
@@ -15,9 +14,6 @@ internal fun HttpClientConfig<*>.HttpLogging() {
             override fun log(message: String) {
                 httpLogger.info { message }
             }
-        }
-        filter { request ->
-            !request.url.encodedPath.contains("seg.so")
         }
     }
 }
