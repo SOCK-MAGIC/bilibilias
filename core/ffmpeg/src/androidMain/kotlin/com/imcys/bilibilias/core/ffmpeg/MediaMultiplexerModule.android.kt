56cc6f5b0b3cffcbefc4d9444e96ec2248df108b
sock_magic <startxingcheng@gmail.com>
2025-09-28 12:03:03 +0800

    Refactor: Simplify MediaMultiplexerModule for Android
    This commit simplifies the Koin module definition for `MediaMultiplexer` on Android. It removes the named qualifier and directly binds `FfmpegMediaMultiplexer` to the `MediaMultiplexer` interface.
    
    Specific changes:
    - In `core/ffmpeg/src/androidMain/kotlin/com/imcys/bilibilias/core/ffmpeg/MediaMultiplexerModule.android.kt`:
        - Removed the commented-out `Media3MediaMultiplexer` binding.
        - Changed the `FfmpegMediaMultiplexer` binding from using a named qualifier ("ffmpeg") to a direct binding using `bind MediaMultiplexer::class`.

--- core/ffmpeg/src/androidMain/kotlin/com/imcys/bilibilias/core/ffmpeg/MediaMultiplexerModule.android.kt
+++ core/ffmpeg/src/androidMain/kotlin/com/imcys/bilibilias/core/ffmpeg/MediaMultiplexerModule.android.kt
@@ -1,18 +1,10 @@
 package com.imcys.bilibilias.core.ffmpeg
 
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.named
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val MediaMultiplexerModule: Module = module {
-//    singleOf(::Media3MediaMultiplexer) {
-//        named("default")
-//        bind<MediaMultiplexer>()
-//    }
-    singleOf(::FfmpegMediaMultiplexer) {
-        named("ffmpeg")
-        bind<MediaMultiplexer>()
-    }
+    singleOf(::FfmpegMediaMultiplexer) bind MediaMultiplexer::class
 }
