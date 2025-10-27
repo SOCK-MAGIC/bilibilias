a029e01202edb27e2e9a4b2436fdfe91d1388baf
sock_magic <startxingcheng@gmail.com>
2025-10-27 20:54:54 +0800

    Build: Add danmaku module dependency to video-player
    This commit updates the Gradle build configuration for the `feature:video-player` module. It adds a new `implementation` dependency on `projects.core.danmaku`.
    
    This change allows the video player feature to access and utilize the functionalities provided by the danmaku module.

--- feature/video-player/build.gradle.kts
+++ feature/video-player/build.gradle.kts
@@ -7,6 +7,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(projects.core.data)
+            implementation(projects.core.danmaku)
             implementation(projects.core.videoPlayer)
         }
     }
