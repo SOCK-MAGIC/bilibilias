3ca3ecbf261a31b061cb639d41e445d99a3c7991
sock_magic <startxingcheng@gmail.com>
2025-11-13 00:05:15 +0800

    Refactor: Add `Path` conversion and directory creation helpers for JVM
    This commit introduces utility functions for JVM to enhance interoperability between `java.io.File` and the `kotlinx.io.files.Path` library.
    
    It adds a `toKtPath()` extension function to convert a `File` to a `Path`, and a `createDirectories` extension function for `SystemPath` which acts as a wrapper around `SystemFileSystem.createDirectories`.
    
    Specific changes:
    - In `core/common/src/jvmMain/kotlin/com/imcys/bilibilias/core/io/Path.jvm.kt`:
        - Added `File.toKtPath()` to convert `java.io.File` to `kotlinx.io.files.Path`.
        - Added `SystemPath.createDirectories()` to expose the underlying `SystemFileSystem` functionality for creating directories.

--- core/common/src/jvmMain/kotlin/com/imcys/bilibilias/core/io/Path.jvm.kt
+++ core/common/src/jvmMain/kotlin/com/imcys/bilibilias/core/io/Path.jvm.kt
@@ -1,11 +1,18 @@
 package com.imcys.bilibilias.core.io
 
 import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
 import java.io.File
 
 fun Path.toFile(): File = File(this.toString())
 fun SystemPath.toFile(): File = path.toFile()
+fun File.toKtPath(): Path = Path(this.toString())
 
+/**
+ * @see FileSystem.createDirectories
+ */
+fun SystemPath.createDirectories(mustCreate: Boolean = false): Unit =
+    SystemFileSystem.createDirectories(path, mustCreate)
 actual val SystemPath.absolutePath: String
     @Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
     get() = path.file.absolutePath
