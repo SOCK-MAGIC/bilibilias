d490a7fbe1b9411e2b6bb0d929c2c8f8063bd2e9
sock_magic <startxingcheng@gmail.com>
2025-11-13 01:56:37 +0800

    Build: Refactor Koin module structure
    This commit refactors the Koin dependency injection setup by consolidating feature modules into the main `allModules` list.
    
    The separate `featureModules` and `otherModules` extension functions have been removed, and their contents are now included directly in the `allModules` function. This simplifies the Koin initialization logic by reducing nesting and improving readability. Additionally, unused code related to a custom `HttpDownloader` and `CoroutineScope` has been deleted.

--- app/src/commonMain/kotlin/com/imcys/bilibilias/Koin.kt
+++ app/src/commonMain/kotlin/com/imcys/bilibilias/Koin.kt
@@ -1,48 +1,26 @@
 package com.imcys.bilibilias
 
-import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import com.imcys.bilibilias.core.data.di.DataModule
 import com.imcys.bilibilias.core.datasource.di.DataSourceModule
 import com.imcys.bilibilias.core.datasource.utils.WbiInitializer
-import com.imcys.bilibilias.core.datastore.asDataStoreSerializer
-import com.imcys.bilibilias.core.datastore.new
-import com.imcys.bilibilias.core.datastore.resolveDataStoreFile
 import com.imcys.bilibilias.core.di.CommonModule
-import com.imcys.bilibilias.core.di.applicationScope
 import com.imcys.bilibilias.core.domain.UseCaseModule
 import com.imcys.bilibilias.core.ffmpeg.MediaProcessorModule
 import com.imcys.bilibilias.core.http.downloader.HttpDownloader
-import com.imcys.bilibilias.core.http.downloader.KtorPersistentHttpDownloader
 import com.imcys.bilibilias.core.http.downloader.di.HttpDownloaderModule
-import com.imcys.bilibilias.core.http.downloader.model.DownloadState
-import com.imcys.bilibilias.core.ktor.client.createHttpClient
-import com.imcys.bilibilias.core.logging.logger
 import com.imcys.bilibilias.di.NavigationModule
 import com.imcys.bilibilias.feature.cache.di.CacheModule
 import com.imcys.bilibilias.feature.search.di.SearchModule
 import com.imcys.bilibilias.feature.settings.di.SettingsModule
 import com.imcys.bilibilias.feature.videoplaayer.di.EpisodePlayerModule
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.request.header
-import io.ktor.http.HttpHeaders
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
-import kotlinx.io.files.Path
-import kotlinx.io.files.SystemFileSystem
-import kotlinx.serialization.builtins.ListSerializer
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.includes
 import org.koin.dsl.module
-import kotlin.time.ExperimentalTime
 
 fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
@@ -65,59 +43,11 @@ fun KoinApplication.commonModules() = module {
         MediaProcessorModule,
         UseCaseModule,
         HttpDownloaderModule,
-        featureModules(),
-        otherModules()
-    )
-}
-
-fun KoinApplication.featureModules() = module {
-    includes(
         CacheModule,
         SearchModule,
         SettingsModule,
         EpisodePlayerModule,
     )
-}
-
-private fun KoinApplication.otherModules() = module {
-    @OptIn(ExperimentalTime::class)
-    single<HttpDownloader> {
-        KtorPersistentHttpDownloader(
-            dataStore = DataStoreFactory.new(
-                ListSerializer(DownloadState.serializer()).asDataStoreSerializer { emptyList() },
-                produceFile = { resolveDataStoreFile("ktor_persistent_http_downloader") },
-                corruptionHandler = ReplaceFileCorruptionHandler { emptyList() },
-                scope = CoroutineScope(applicationScope.coroutineContext + Dispatchers.IO),
-            ),
-            client = createHttpClient {
-                defaultRequest {
-                    header(HttpHeaders.Referrer, "https://www.bilibili.com")
-                }
-                Logging {
-                    level = LogLevel.HEADERS
-                    logger = object : io.ktor.client.plugins.logging.Logger {
-                        private val logger =
-                            logger<HttpDownloader>()
-
-                        override fun log(message: String) {
-                            logger.info { message }
-                        }
-                    }
-                }
-            },
-            fileSystem = SystemFileSystem,
-            baseSaveDir = Path(BuildConfig.MEDIA_DOWNLOAD),
-        )
-    }
-    single<CoroutineScope> {
-        CoroutineScope(
-            CoroutineExceptionHandler { coroutineContext, throwable ->
-                logger("ApplicationScope").warn(throwable) {
-                    "Uncaught exception in coroutine $coroutineContext"
-                }
-            } + SupervisorJob() + Dispatchers.IO,
-        )
-    }
 }
 
 fun KoinApplication.startCommonKoinModule(
