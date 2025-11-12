b6d454b8a80d824238e056fad994ca145fdd91aa
sock_magic <startxingcheng@gmail.com>
2025-11-13 00:04:08 +0800

    Refactor: Update dependencies in `http-downloader` module
    This commit refactors the dependencies within the `core/http-downloader` module's `build.gradle.kts` file.
    
    The `bilibilias.android.room` plugin has been removed, and the `api` dependency on `core.common` has been changed to `implementation`. Additionally, a new `api` dependency on `androidx.datastore` has been introduced.
    
    Specific changes:
    - Removed the `bilibilias.android.room` plugin.
    - Changed `api(projects.core.common)` to `implementation(projects.core.common)`.
    - Added `api(libs.androidx.datastore)`.

--- core/http-downloader/build.gradle.kts
+++ core/http-downloader/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     alias(libs.plugins.bilibilias.kmp.library)
-    alias(libs.plugins.bilibilias.android.room)
     alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
     sourceSets {
         commonMain.dependencies {
-            api(projects.core.common)
             api(projects.core.ktorClient)
+            implementation(projects.core.common)
+
+            api(libs.androidx.datastore)
         }
     }
 }
